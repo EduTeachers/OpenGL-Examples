081e35af26d70280ea762c893cc6c6b441c73328
Samuel <Samuel@DESKTOP-N6SD6B7>
2021-04-27 15:28:26 +0200

    Textures

---
+++ Textures/src/main/java/educanet/Game.java
@@ -0,0 +1,153 @@
+package educanet;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL33;
+import org.lwjgl.stb.STBImage;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+public class Game {
+
+    private static final float[] vertices = {
+            0.5f, 0.5f, 0.0f, // 0 -> Top right
+            0.5f, -0.5f, 0.0f, // 1 -> Bottom right
+            -0.5f, -0.5f, 0.0f, // 2 -> Bottom left
+            -0.5f, 0.5f, 0.0f, // 3 -> Top left
+    };
+
+    private static final float[] colors = {
+            1.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 0.0f,
+    };
+
+    private static final float[] textures = {
+            1.0f, 1.0f,
+            1.0f, 0.0f,
+            0.0f, 0.0f,
+            0.0f, 1.0f,
+    };
+
+    private static final int[] indices = {
+            0, 1, 3, // First triangle
+            1, 2, 3 // Second triangle
+    };
+
+    private static int squareVaoId;
+    private static int squareVboId;
+    private static int squareEboId;
+    private static int colorsId;
+    private static int textureIndicesId;
+
+    private static int textureId;
+
+    public static void init(long window) {
+        // Setup shaders
+        Shaders.initShaders();
+
+        // Generate all the ids
+        squareVaoId = GL33.glGenVertexArrays();
+        squareVboId = GL33.glGenBuffers();
+        squareEboId = GL33.glGenBuffers();
+        colorsId = GL33.glGenBuffers();
+        textureIndicesId = GL33.glGenBuffers();
+
+        textureId = GL33.glGenTextures();
+        loadImage();
+
+        // Tell OpenGL we are currently using this object (vaoId)
+        GL33.glBindVertexArray(squareVaoId);
+
+        // Tell OpenGL we are currently writing to this buffer (eboId)
+        GL33.glBindBuffer(GL33.GL_ELEMENT_ARRAY_BUFFER, squareEboId);
+        IntBuffer ib = BufferUtils.createIntBuffer(indices.length)
+                .put(indices)
+                .flip();
+        GL33.glBufferData(GL33.GL_ELEMENT_ARRAY_BUFFER, ib, GL33.GL_STATIC_DRAW);
+
+        // Change to VBOs...
+        // Tell OpenGL we are currently writing to this buffer (vboId)
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, squareVboId);
+
+        FloatBuffer fb = BufferUtils.createFloatBuffer(vertices.length)
+                .put(vertices)
+                .flip();
+
+        // Send the buffer (positions) to the GPU
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, fb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(0, 3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(0);
+
+        // Clear the buffer from the memory (it's saved now on the GPU, no need for it here)
+        MemoryUtil.memFree(fb);
+
+        // Change to Color...
+        // Tell OpenGL we are currently writing to this buffer (colorsId)
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, colorsId);
+
+        FloatBuffer cb = BufferUtils.createFloatBuffer(colors.length)
+                .put(colors)
+                .flip();
+
+        // Send the buffer (positions) to the GPU
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, cb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(1, 3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(1);
+
+        // Change to Textures...
+        // Tell OpenGL we are currently writing to this buffer (colorsId)
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, textureIndicesId);
+
+        FloatBuffer tb = BufferUtils.createFloatBuffer(textures.length)
+                .put(textures)
+                .flip();
+
+        // Send the buffer (positions) to the GPU
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, tb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(2, 2, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(2);
+
+        // Clear the buffer from the memory (it's saved now on the GPU, no need for it here)
+        MemoryUtil.memFree(cb);
+        MemoryUtil.memFree(tb);
+    }
+
+    public static void render(long window) {
+        GL33.glUseProgram(Shaders.shaderProgramId);
+
+        // Draw using the glDrawElements function
+        GL33.glBindTexture(GL33.GL_TEXTURE_2D, textureId);
+        GL33.glBindVertexArray(squareVaoId);
+        GL33.glDrawElements(GL33.GL_TRIANGLES, indices.length, GL33.GL_UNSIGNED_INT, 0);
+    }
+
+    public static void update(long window) {
+
+    }
+
+    private static void loadImage() {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer w = stack.mallocInt(1);
+            IntBuffer h = stack.mallocInt(1);
+            IntBuffer comp = stack.mallocInt(1);
+
+            ByteBuffer img = STBImage.stbi_load("resources/textures/img.png", w, h, comp, 3);
+            if (img != null) {
+                img.flip();
+
+                GL33.glBindTexture(GL33.GL_TEXTURE_2D, textureId);
+                GL33.glTexImage2D(GL33.GL_TEXTURE_2D, 0, GL33.GL_RGB, w.get(), h.get(), 0, GL33.GL_RGB, GL33.GL_UNSIGNED_BYTE, img);
+                GL33.glGenerateMipmap(GL33.GL_TEXTURE_2D);
+
+                STBImage.stbi_image_free(img);
+            }
+        }
+    }
+
+}
